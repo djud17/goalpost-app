d2db79925cfd8aeb17f9b8858dfdf2d56e5a3417
djud17 <davidtonoyan10@rambler.ru>
2021-05-04 12:54:08 +0300

    Initial Commit

---
+++ goal-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goal-app
+//
+//  Created by Давид Тоноян  on 04.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
